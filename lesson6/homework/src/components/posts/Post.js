c804af6ee098ff423559dfb352663d9ba0e66eb8
alchimikus <drizer84@gmail.com>
2021-09-01 18:03:26 +0300

    Add correction for UserDetail and create Post

---
+++ lesson6/homework/src/components/posts/Post.js
@@ -0,0 +1,6 @@
+export default function Post({item}){
+    return(
+        
+    )
+
+}
--- lesson6/homework/src/components/users/UserDetails.js
+++ lesson6/homework/src/components/users/UserDetails.js
@@ -3,20 +3,24 @@ import {getUserById} from "../../services/jsonplaceholder.api.services";
 
 export default function UserDetails({match:{params:{id}}}){
 
-    let [userDetail, setUserDetail] = useState({});
-    useEffect(()=>{
-        getUserById(id).then(value => {
-            setUserDetail(value)});
+    let [userDetail, setUserDetail] = useState(undefined);
+    useEffect(() => {
+        async function fetchData(){
+            let response = await  getUserById(id);
+            await setUserDetail({...response});
+        }
+        fetchData()
+
     },[id]);
     return(
         <div className={'user_details_box'}>
-
+            { userDetail && (<div>
             <p><strong>Name: </strong>{userDetail.name}</p>
             <p><strong>Email: </strong>{userDetail.email}</p>
             <p><strong>Company: </strong>{userDetail.company.name}</p>
             <p><strong>City: </strong>{userDetail.address.city}</p>
             <p><strong>Phone: </strong>{userDetail.phone}</p>
-            <p><strong>Website: </strong>{userDetail.website}</p>
+            <p><strong>Website: </strong>{userDetail.website}</p> </div>)}
         </div>
     )
 }
