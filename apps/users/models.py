92a42464411e6a3e2378837011571d3f6372c230
alchimikus <drizer84@gmail.com>
2021-11-06 12:55:22 +0200

    start commit

---
+++ apps/users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.db import models
+
+# Create your models here.
+from apps.users.managers import UserManager
+
+
+class UserModel(AbstractUser, PermissionsMixin):
+    class Meta:
+        db_table = 'auth_user'
+
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
